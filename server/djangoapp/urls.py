b096d4026fa07b60a207180e144a4bc2f0902114
kimalysha93 <kimalysha93@gmail.com>
2025-03-07 21:45:51 -0500

    added dealer pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,12 +20,12 @@ urlpatterns = [
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for get_dealers
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
 
     # path for get_dealers by state
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
-    # path for dealer reviews view
+    # path for dealer details
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     # path for get dealer reviews
