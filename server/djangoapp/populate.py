9b5bc6058c0e800bb9462724267426e99e4ca54c
kimalysha93 <kimalysha93@gmail.com>
2025-03-07 23:28:29 -0500

    python lint

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,5 +1,6 @@
 from .models import CarMake, CarModel
 
+
 def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
@@ -7,7 +8,7 @@ def initiate():
         {"name": "Audi", "description": "Great cars. German technology"},
         {"name": "Kia", "description": "Great cars. Korean technology"},
         {"name": "Toyota", "description": "Great cars. Japanese technology"},
-    ]
+        ]
 
     car_make_instances = []
     for data in car_make_data:
@@ -58,65 +59,65 @@ def initiate():
       },
       {
         "name": "A4",
-        "type":"SUV",
+        "type": "SUV",
         "year": 2023,
         "car_make": car_make_instances[2]
       },
       {
         "name": "A5",
-        "type":"SUV",
+        "type": "SUV",
         "year": 2023,
         "car_make": car_make_instances[2]
       },
       {
         "name": "A6",
-        "type":"SUV",
+        "type": "SUV",
         "year": 2023,
         "car_make": car_make_instances[2]
       },
       {
         "name": "Sorrento",
-        "type":"SUV",
+        "type": "SUV",
         "year": 2023,
         "car_make": car_make_instances[3]
       },
       {
         "name": "Carnival",
-        "type":"SUV",
+        "type": "SUV",
         "year": 2023,
         "car_make": car_make_instances[3]
       },
       {
         "name": "Cerato",
-        "type":"Sedan",
+        "type": "Sedan",
         "year": 2023,
         "car_make": car_make_instances[3]
       },
       {
         "name": "Corolla",
-        "type":"Sedan",
+        "type": "Sedan",
         "year": 2023,
         "car_make": car_make_instances[4]
       },
       {
         "name": "Camry",
-        "type":"Sedan",
+        "type": "Sedan",
         "year": 2023,
         "car_make": car_make_instances[4]
       },
       {
         "name": "Kluger",
-        "type":"SUV",
+        "type": "SUV",
         "year": 2023,
         "car_make": car_make_instances[4]
       },
       # Add more CarModel instances as needed
-    ]
+      ]
 
     for data in car_model_data:
       CarModel.objects.create(
-        name = data['name'],
-        car_make = data['car_make'],
-        type = data['type'],
-        year = data['year']
+        name=data['name'],
+        car_make=data['car_make'],
+        type=data['type'],
+        year=data['year']
       )
